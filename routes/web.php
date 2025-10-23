2f3e79df68de7305292fbe654fe7ac7b9f193250
RattleT2 <mario.pratama2016@gmail.com>
2025-10-23 15:20:53 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [App\Http\Controllers\LampController::class, 'index'])->name('lamps.index');
+
+Route::post('/lamps/{id}', [App\Http\Controllers\LampController::class, 'updateLamp'])->name('lamps.update');
